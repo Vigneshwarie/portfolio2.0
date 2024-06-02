eeae16d985fec27fbf0e08a54cf3a694dd8b8f46
Vigneswari Sambandam <vigneshwarie@gmail.com>
2024-06-02 01:27:43 -0700

    Separated Navbar section and added styles

--- myportfolio/src/components/Header.jsx
+++ myportfolio/src/components/Header.jsx
@@ -1,24 +1,14 @@
 import Container from 'react-bootstrap/Container';
+import NavbarMenu from "./NavbarMenu";
 
 function Header() {
      return (
           <Container>
-               <h1><a href="index.html">Vigneswari Sambandam</a></h1>
-               <h3>a Master's graduate, passionate about designing and developing full-stack applications</h3>
-
-               <nav id="navbar" class="navbar">
-                    <ul>
-                         <li><a class="nav-link active" href="#header">Home</a></li>
-                         <li><a class="nav-link" href="#about">About</a></li>
-                         <li><a class="nav-link" href="#experience">Experience</a></li>
-                         <li><a class="nav-link" href="#projects">Projects</a></li>
-                         <li><a class="nav-link" href="#skills">Skills</a></li>
-                         <li><a class="nav-link" href="#awards">Awards</a></li>
-                         <li><a class="nav-link" href="#contact">Contact</a></li>
-                    </ul>
-                    <i class="bi bi-list mobile-nav-toggle"></i>
-               </nav>
-
+               <header id="header">
+                    <h1><a href="index.html">Vigneswari Sambandam</a></h1>
+                    <h3>a Master's graduate, passionate about designing and developing full-stack applications</h3>
+                    <NavbarMenu/>
+               </header>
           </Container>
      );
 }
