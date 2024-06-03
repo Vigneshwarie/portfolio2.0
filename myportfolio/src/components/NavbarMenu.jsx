c57343d612613423557b3609d223e147cb571669
Vigneswari Sambandam <vigneshwarie@gmail.com>
2024-06-02 18:48:41 -0700

    Updated theme and styling of the site

--- myportfolio/src/components/NavbarMenu.jsx
+++ myportfolio/src/components/NavbarMenu.jsx
@@ -1,25 +1,24 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import '../assets/styles/Navbar.css';
 
 
 function NavbarMenu() {
      return (
-          <Navbar expand="lg" className="bg-body-tertiary">
-               <Container>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                         <Nav className="me-auto">
-                              <Nav.Link href="#home">Home</Nav.Link>
-                              <Nav.Link href="#about">About</Nav.Link>
-                              <Nav.Link href="#experience">Experience</Nav.Link>
-                              <Nav.Link href="#projects">Projects</Nav.Link>
-                              <Nav.Link href="#skills">Skills</Nav.Link>
-                              <Nav.Link href="#awards">Awards</Nav.Link>
-                              <Nav.Link href="#contact">Contact</Nav.Link>
-                         </Nav>
-                    </Navbar.Collapse>
-               </Container>
+          <Navbar expand="lg" className="custom-navbar">
+               <Navbar.Toggle aria-controls="basic-navbar-nav" />
+               <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                         <Nav.Link href="#home">Home</Nav.Link>
+                         <Nav.Link href="#about">About</Nav.Link>
+                         <Nav.Link href="#experience">Experience</Nav.Link>
+                         <Nav.Link href="#projects">Projects</Nav.Link>
+                         <Nav.Link href="#skills">Skills</Nav.Link>
+                         <Nav.Link href="#awards">Awards</Nav.Link>
+                         <Nav.Link href="#contact">Contact</Nav.Link>
+                    </Nav>
+               </Navbar.Collapse>
           </Navbar>
      );
 }
