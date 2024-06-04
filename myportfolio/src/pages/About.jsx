60e4656b3044e47d0176484fac944906ee296834
Vigneswari Sambandam <vigneshwarie@gmail.com>
2024-06-04 15:56:43 -0700

    Added AOS for transition and changed theme for the site

--- myportfolio/src/pages/About.jsx
+++ myportfolio/src/pages/About.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import Collapse from 'react-bootstrap/Collapse';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function About() {
-
+     useEffect(() => {
+          AOS.init();
+     }, []
+     );
 
      return (
           <>
 
-               <div id="about">
+               <div id="about" data-aos="fade-up" data-aos-delay="100">
                     Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
                     terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
                     labore wes anderson cred nesciunt sapiente ea proident.
--- myportfolio/src/pages/Awards.jsx
+++ myportfolio/src/pages/Awards.jsx
@@ -1,7 +1,16 @@
+import { useState, useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 function Awards() {
+     useEffect(() => {
+          AOS.init();
+     }, []
+     );
+     
      return (
-          <section className="awards">
-               <div>
+          <section className="awards" >
+               <div data-aos="fade-up" data-aos-delay="100">
                     <h3>Awards</h3>
                     <p>This is the awards page.</p>
                </div>
