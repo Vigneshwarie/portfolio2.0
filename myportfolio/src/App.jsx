1a92de4d043887fc71c6cc07d0e098ea11846adb
Vigneswari Sambandam <vigneshwarie@gmail.com>
2024-06-05 16:13:43 -0700

    Fixed Navbar and utilized router-dom for navigation and split the logic

--- myportfolio/src/App.jsx
+++ myportfolio/src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
 import Header from './components/Header';
-import Footer from './components/Footer';
+//import Footer from './components/Footer';
 import NavbarMenu from "./components/NavbarMenu";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { Outlet } from 'react-router-dom';
 import './App.css'
 
 function App() {
@@ -12,6 +10,7 @@ function App() {
 			<div>
 				<Header />
 				<NavbarMenu />
+				<Outlet />
       		</div>
     		</>
   	)
--- myportfolio/src/components/NavbarMenu.jsx
+++ myportfolio/src/components/NavbarMenu.jsx
@@ -1,65 +1,27 @@
 import React, { useState } from 'react';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import { Navbar, Nav } from "react-bootstrap";
+import { Link, useLocation } from 'react-router-dom';
 import '../assets/styles/Navbar.css';
-import About from '../pages/About';
-import Experience from '../pages/Experience';
-import Projects from '../pages/Projects';
-import Skills from '../pages/Skills';
-import Awards from '../pages/Awards';
-import Contact from '../pages/Contact';
 import Container from 'react-bootstrap/Container';
 
 
 function NavbarMenu() {
-     const [activePage, setActivePage] = useState(true);
-
-     const renderPage = () => {
-          switch (activePage) {
-               case 'about':
-                    return <About />;
-               case 'experience':
-                    return <Experience />;
-               case 'projects':
-                    return <Projects />;
-               case 'skills':
-                    return <Skills />;
-               case 'awards':
-                    return <Awards />;
-               case 'contact':
-                    return <Contact />;
-               default:
-                    return;
-          }
-     };
+     const currentPage = useLocation().pathname;
      return (
           <Container>
-               <Navbar expand="lg" className="custom-navbar">
+               <Navbar collapseOnSelect={true} expand="lg" className="custom-navbar">
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                          <Nav variant="underline" className="me-auto">
-                              <Nav.Link href="#about" onClick={() => {
-                                   setActivePage('about');
-                              }}>About</Nav.Link>
-                              <Nav.Link href="#experience" onClick={() => {
-                                   setActivePage('experience');
-                              }}>Experience</Nav.Link>
-                              <Nav.Link href="#projects" onClick={() => {
-                                   setActivePage('projects');
-                              }}>Projects</Nav.Link>
-                              <Nav.Link href="#skills" onClick={() => {
-                                   setActivePage('skills');
-                              }}>Skills</Nav.Link>
-                              <Nav.Link href="#awards" onClick={() => {
-                                   setActivePage('awards');
-                              }}>Awards</Nav.Link>
-                              <Nav.Link href="#contact" onClick={() => {
-                                   setActivePage('contact');
-                              }}>Contact</Nav.Link>
+                              <Nav.Link as={Link} to="/About" eventKey="/About" className={currentPage === "/About" ? 'nav-link active' : 'nav-link'}>About</Nav.Link>
+                              <Nav.Link as={Link} to="/Experience" eventKey="/Experience">Experience</Nav.Link>
+                              <Nav.Link as={Link} to="/Projects" eventKey="/Projects">Projects</Nav.Link>
+                              <Nav.Link as={Link} to="/Skills" eventKey="/Skills">Skills</Nav.Link>
+                              <Nav.Link as={Link} to="/Awards" eventKey="/Awards">Awards</Nav.Link>
+                              <Nav.Link as={Link} to="/Contact" eventKey="/Contact">Contact</Nav.Link>
                          </Nav>
                     </Navbar.Collapse>
                </Navbar>
-               {renderPage()}
           </Container>   
      );
 }
