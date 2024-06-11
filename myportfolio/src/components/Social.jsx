9723c75f22811ffd6d08984a3fec7e1e65117927
Vigneswari Sambandam <vigneshwarie@gmail.com>
2024-06-10 20:06:31 -0700

    Added position parameter to tooltip component and added tooltips to all links

--- myportfolio/src/components/Social.jsx
+++ myportfolio/src/components/Social.jsx
@@ -1,5 +1,4 @@
 import Container from 'react-bootstrap/Container';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import '../assets/styles/Social.css';
 import Tooltips from './Tooltips';
 
@@ -10,8 +9,8 @@ function Social() {
                <div className="social-links">
                     <Tooltips message={`GitHub`} children={<a href="https://github.com/Vigneshwarie/" className="github" target="_blank">
                          <i className="bi bi-github"></i>
-                         </a>} />
-                    <Tooltips message={`LinkedIn`} children={<a href="https://www.linkedin.com/in/vigneswari-sambandam-37b03730/" className="linkedin" target="_blank"><i className="bi bi-linkedin"></i></a>} />
+                         </a>} position="bottom"/>
+                    <Tooltips message={`LinkedIn`} children={<a href="https://www.linkedin.com/in/vigneswari-sambandam-37b03730/" className="linkedin" target="_blank"><i className="bi bi-linkedin"></i></a>} position="bottom"/>
                </div>
           </Container>
      );
--- myportfolio/src/components/Tooltips.jsx
+++ myportfolio/src/components/Tooltips.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { Tooltip, OverlayTrigger } from 'react-bootstrap';
 import '../assets/styles/Tooltip.css';
 
-const Tooltips = ({message, children}) => {
+const Tooltips = ({message, children, position}) => {
      return (
           <OverlayTrigger
-               placement="bottom"
+               placement={position}
                overlay={<Tooltip id="button-tooltip">{message}</Tooltip>}
           >
                {React.Children.only(children)}
